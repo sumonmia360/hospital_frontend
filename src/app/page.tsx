57b54fa0bb5bfbf02be06067791f426dd477db2d
sumonmia360 <sumonmia4526@gmail.com>
2025-11-10 15:18:39 +0600

    hydration problem fix done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import HumanBody from "@/components/home/HumanBody";
-import HumanBodyFemale from "@/components/home/HumanBodyFemale";
+"use client";
+
+
 import Navber from "@/components/navber/Navber";
 
+import dynamic from 'next/dynamic'
+ 
+const HumanBody = dynamic(() => import('@/components/home/HumanBody'), { ssr: false })
+
+
+
 export default function Home() {
+  // const [isClient, setIsClient] = useState(false)
+
+  
   return (
     <div className="min-h-screen  mx-auto  bg-white font-sans  ">
       <header>
@@ -11,8 +21,9 @@ export default function Home() {
       <main>
         <section className="max-w-7xl mx-auto border">
           <div className="flex items-center justify-between mb-100 border border-red-600 h-[1000px]">
-            <HumanBody></HumanBody>
-            {/* <HumanBodyFemale></HumanBodyFemale> */}
+        
+              <HumanBody />
+          
           </div>
         </section>
       </main>
--- src/components/navber/HeroBadge.tsx
+++ src/components/navber/HeroBadge.tsx
@@ -15,7 +15,7 @@ export default function HeroBadge() {
       </Link>
       <Link href={'/'} className="flex  bg-[#159EEC] p-4 h-20 rounded-lg shadow-lg gap-3 items-center ">
         <p className="text-white">Our Services - Go to Full pages</p>
-        <Image src={"/images/cashSVG.svg"} alt="calender svg" width={30} height={30}></Image>
+       <Image src={"/images/cashSVG.svg"} alt="Cash svg" width={30} height={30} className=""></Image>
       </Link>
     </div>
   );

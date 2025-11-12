947ac37c6ab95077280bca44f2dd04c977a8a9eb
sumonmia360 <sumonmia4526@gmail.com>
2025-11-12 11:23:36 +0600

    clone svg file done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-
+import HumanBodyFemale from "@/components/home/HumanBodyFemale";
 import Navber from "@/components/navber/Navber";
 
-import dynamic from 'next/dynamic'
- 
-const HumanBody = dynamic(() => import('@/components/home/HumanBody'), { ssr: false })
+import dynamic from "next/dynamic";
 
-
+const HumanBody = dynamic(() => import("@/components/home/HumanBody"), {
+  ssr: false,
+});
 
 export default function Home() {
   // const [isClient, setIsClient] = useState(false)
 
-  
   return (
     <div className="min-h-screen  mx-auto  bg-white font-sans  ">
       <header>
@@ -21,9 +20,9 @@ export default function Home() {
       <main>
         <section className="max-w-7xl mx-auto border">
           <div className="flex items-center justify-between mb-100 border border-red-600 h-[1000px]">
-        
-              <HumanBody />
-          
+            <HumanBodyFemale></HumanBodyFemale>
+
+            {/* <HumanBody /> */}
           </div>
         </section>
       </main>
